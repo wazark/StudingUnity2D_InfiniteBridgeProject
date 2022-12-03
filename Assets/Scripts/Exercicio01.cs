61ccbf7ea143427d06c7d5ca750f3a3bb0d2b4a0
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-03 18:57:57 +0000

    execicio01
    colocar algumas variáveis e criar uma função.

---
+++ Assets/Scripts/Exercicio01.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio01 : MonoBehaviour
+{
+    //Personagem de jogo
+
+    int levelInitial = 1;
+    int levelMax = 100;
+    int currentExp = 0;
+    int expToNextLevel = 100;
+    string playerName = "Diony";
+    string playerClass= "none"
+    string currentMap= "map_initial"
+    bool isLevel5 = false;
+    bool isWarrior = false;
+    bool isMage= false;
+    bool isPK= false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void levelUp()
+    {
+        
+
+    }
+}
