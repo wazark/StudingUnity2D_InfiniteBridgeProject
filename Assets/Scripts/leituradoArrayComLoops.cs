6eaeb91a57577dd43f0847adb905fa0c690749eb
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-04 13:56:05 +0000

    Update leituradoArrayComLoops.cs

--- Assets/Scripts/leituradoArrayComLoops.cs
+++ Assets/Scripts/leituradoArrayComLoops.cs
@@ -7,7 +7,8 @@ public class leituradoArrayComLoops : MonoBehaviour
     public float[] fundamentoArray;
     void Start()
     {
-        
+        /* Utilizando o While:
+
         int indice =0; //variavel declarada 
         while (indice < fundamentoArray.Length) // enquanto indice for menor que o tamanho da variavel array, vai ficar executando a função do print mostrando em cada print o valor que foi definido na array. Ou seja, no 0 vai mostrar qual valor foi dado ao lenght 0.
         {
@@ -15,6 +16,16 @@ public class leituradoArrayComLoops : MonoBehaviour
             print(fundamentoArray[indice]);
             indice +=1;
         }
+
+        */
+
+        // Utilizando o For:
+
+        for (int indice = 0; indice < fundamentoArray.Length; indice++)
+        {
+            print(fundamentoArray[indice]);
+        }
+
     }
 
     // Update is called once per frame
